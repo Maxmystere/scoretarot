b17296ad16a889abfb17d984c31746d0229cabc7
seraf1 <seraf1@2d6739f2-d8af-4b0d-8089-700d66601465>
2012-12-06 21:42:24 +0000

    Modification Cellule Donnes
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/scoretarot/android@7 2d6739f2-d8af-4b0d-8089-700d66601465

--- src/fr/pssoftware/scoretarot/TableDonneCell.java
+++ src/fr/pssoftware/scoretarot/TableDonneCell.java
@@ -2,13 +2,15 @@ package fr.pssoftware.scoretarot;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-/**
- * TODO: document your custom view class.
- */
 public class TableDonneCell extends FrameLayout {
 	private int contrat=0;
 	private int points=0;
@@ -18,7 +20,15 @@ public class TableDonneCell extends FrameLayout {
 	private int petit=0;
 	private int poignee=0;
 	private int chelem=0;
-
+	private RelativeLayout wFond;
+	private TextView wPoints;
+	private TextView wTotalPoints;
+	private TextView wPetit;
+	private TextView wPoignee;
+	private TextView wScore;
+	private TextView wChelem;
+	private ImageView wImg;
+	
 	public TableDonneCell(Context context) {
 		super(context);
 		init(context,null, 0);
@@ -49,12 +59,17 @@ public class TableDonneCell extends FrameLayout {
 		poignee = a.getInt(R.styleable.TableDonneCell_poignee, 0);
 		chelem = a.getInt(R.styleable.TableDonneCell_chelem, 0);
 		a.recycle();
-		maj();
+		
+		wFond=(RelativeLayout) findViewById(R.id.tdc_fond);
+		wImg=(ImageView) findViewById(R.id.tdc_img);
+		wPoints=(TextView) findViewById(R.id.tdc_points);
+		wTotalPoints=(TextView) findViewById(R.id.tdc_total_points);
+		wScore=(TextView) findViewById(R.id.tdc_score);
+		wPetit=(TextView) findViewById(R.id.tdc_petit);
+		wPoignee=(TextView) findViewById(R.id.tdc_poignee);
+		wChelem=(TextView) findViewById(R.id.tdc_chelem);
 	}
 	
-	private void maj(){
-		
-	}
 
 	public int getContrat() {
 		return contrat;
@@ -62,7 +77,32 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setContrat(int contrat) {
 		this.contrat = contrat;
-		maj();
+
+		String tr="";
+		switch(contrat){
+		case 0:
+			tr="00";
+			break;
+		case 1:
+			tr="44";
+			break;
+		case 2:
+			tr="88";
+			break;
+		case 3:
+			tr="AA";
+			break;
+		case 4:
+			tr="EE";
+			break;
+		}
+		if (total_points<0){
+			tr="#"+tr+"0000";
+		}else if (total_points>=0){
+			tr="#00"+tr+"00";
+			
+		}
+		wFond.setBackgroundColor(Color.parseColor(tr));
 	}
 
 	public int getPoints() {
@@ -71,7 +111,7 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setPoints(int points) {
 		this.points = points;
-		maj();
+		wPoints.setText(points);
 	}
 
 	public int getTotal_Points() {
@@ -80,7 +120,7 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setTotal_Points(int points) {
 		this.total_points = points;
-		maj();
+		wTotalPoints.setText(points);
 	}
 	public int getScore() {
 		return score;
@@ -88,7 +128,7 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setScore(int score) {
 		this.score = score;
-		maj();
+		wScore.setText(score);
 	}
 
 	public int getRole() {
@@ -97,7 +137,16 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setRole(int role) {
 		this.role = role;
-		maj();
+		switch (role){
+		case -1:
+			wImg.setImageResource(R.drawable.ic_mort);
+		case 0:
+			wImg.setVisibility(View.INVISIBLE);
+		case 1:
+			wImg.setImageResource(R.drawable.ic_appele);
+		case 2:
+			wImg.setImageResource(R.drawable.ic_preneur);
+		}
 	}
 
 	public int getPetit() {
@@ -106,16 +155,40 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setPetit(int petit) {
 		this.petit = petit;
-		maj();
+		if (petit<0) {
+			wPetit.setText("-1");
+		}else if(petit>0){
+			wPetit.setText("+1");
+		}
 	}
 
 	public int getPoignee() {
 		return poignee;
-}
+	}
 
 	public void setPoignee(int poignee) {
 		this.poignee = poignee;
-		maj();
+		switch(poignee){
+		case -3:
+			wPoignee.setText("-PPP");
+			break;
+		case -2:
+			wPoignee.setText("-PP");
+			break;
+		case -1:
+			wPoignee.setText("-P");
+			break;
+		case 1:
+			wPoignee.setText("+P");
+			break;
+		case 2:
+			wPoignee.setText("+PP");
+			break;
+		case 3:
+			wPoignee.setText("+PPP");
+			break;
+		}
+		
 	}
 
 	public int getChelem() {
@@ -124,6 +197,16 @@ public class TableDonneCell extends FrameLayout {
 
 	public void setChelem(int chelem) {
 		this.chelem = chelem;
-		maj();
+		switch(chelem){
+		case -1:
+			wChelem.setText("-C");
+			break;
+		case 1:
+			wChelem.setText("+C");
+			break;
+		case 2:
+			wChelem.setText("+C");
+			break;
+		}
 	}
 }
