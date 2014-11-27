0dd8600a5e7b7ed457c5f03480dbecce5be72a53
seraf1 <philippe.seraphin56@gmail.com>
2014-11-27 15:50:09 +0100

    All (cherry picked from commit f710971)

--- app/build/generated/source/buildConfig/debug/fr/pssoftware/scoretarot/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/fr/pssoftware/scoretarot/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "fr.pssoftware.scoretarot";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 20;
-  public static final String VERSION_NAME = "2.0";
+  public static final int VERSION_CODE = 21;
+  public static final String VERSION_NAME = "2.1";
   /**
    * @deprecated Use {@link #APPLICATION_ID}
    */
